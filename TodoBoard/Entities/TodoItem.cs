2be35f673f067bd3ffe8d05a7c97cb4c851c4257
Jakub Świętek <jakub.swietek@gmail.com>
2018-03-14 11:18:56 +0100

    Added todo item entity

---
+++ TodoBoard/Entities/TodoItem.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace TodoBoard.Entities
+{
+	public enum TodoItemState { Todo, Working, Done }
+
+	public class TodoItem
+	{
+		public int Id { get; set; }
+		public string Name { get; set; }
+		public string Description { get; set; }
+		public TodoItemState State { get; set; }
+	}
+}
