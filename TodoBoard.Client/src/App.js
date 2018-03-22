e06de0b6ded46a9ac179766fa70dc2d101cecc1c
Jakub Świętek <jakub.swietek@gmail.com>
2018-03-22 14:47:44 +0100

    Added react-bootstrap

--- TodoBoard.Client/src/App.js
+++ TodoBoard.Client/src/App.js
@@ -1,11 +1,25 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
+import { Grid, Row, Col, Button } from 'react-bootstrap'
 import './App.css';
 
 class App extends Component {
     render() {
         return (
-      
+            <Grid fluid>
+                <Row>
+                    <div className="hidden"></div>
+                </Row>
+                <Row>
+                    <Col sm={2}></Col>
+                    <Col sm={8} className="text-center">
+                        COS
+                    </Col>
+                    <Col sm={2}></Col>
+                </Row>
+                <Row>
+                    <div className="hidden"></div>
+                </Row>
+            </Grid >
         );
     }
 }
