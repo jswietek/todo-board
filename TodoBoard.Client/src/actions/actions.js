7d07869c6714d15ae4a418fca7a326ebeec8d7a9
Jakub Świętek <jakub.swietek@gmail.com>
2018-03-30 23:32:59 +0200

    Added actions for items and boards. Added reducers for items

---
+++ TodoBoard.Client/src/actions/actions.js
@@ -0,0 +1,14 @@
+﻿import './action-types';
+
+//ITEMS
+export const addItem = item => ({ type: ADD_ITEM, item });
+export const deleteItem = id => ({ type: DELETE_ITEM, id });
+export const changeItemName = (id, newName) => ({ type: CHANGE_ITEM_NAME, id, newName });
+export const changeItemDescription = (id, newDescription) => ({ type: CHANGE_ITEM_DESC, id, newDescription });
+export const moveItem = (id, newBoardId) => ({ type: MOVE_ITEM, id, newBoardId });
+
+
+//BOARDS
+export const addBoard = board => ({ type: ADD_BOARD, board });
+export const deleteBoard = id => ({ type: DELETE_BOARD, id });
+export const changeBoardName = (id, newBoardName) => ({ type: CHANGE_BOARD_NAME, id, newBoardName })
---
+++ TodoBoard.Client/src/reducers/items-reducer.js
@@ -0,0 +1,59 @@
+﻿import { ADD_ITEM, DELETE_ITEM, CHANGE_ITEM_NAME, CHANGE_ITEM_DESC, MOVE_ITEM } from './action-types';
+
+const initialState = {
+    items: [],
+    boards: []
+}
+
+function itemsReducer(state = initialState, action) {
+    switch (action) {
+        case ADD_ITEM:
+            return Object.assign({}, state, {
+                items: [
+                    ...state.items,
+                    action.item
+                ]
+            });
+        case DELETE_ITEM:
+            var itemIdx = state.items.findIndex(item => item.id === action.id);
+            return Object.assign({}, state, {
+                items: [
+                    ...state.items.slice(0, itemIdx),
+                    ...state.slice(itemIdx + 1)
+                ]
+            });
+        case CHANGE_ITEM_NAME:
+            return Object.assign({}, state, {
+                items: state.items.map(item => {
+                    if (item.id === action.id) {
+                        return Onject.assign({}, item, {
+                            name: action.newName,
+                        })
+                    }
+                })
+            });
+        case CHANGE_ITEM_DESC:
+            return Object.assign({}, state, {
+                items: state.items.map(item => {
+                    if (item.id === action.id) {
+                        return Onject.assign({}, item, {
+                            descrption: action.newDescrption,
+                        })
+                    }
+                })
+            });
+        case MOVE_ITEM:
+            return Object.assign({}, state, {
+                items: state.items.map(item => {
+                    if (item.id === action.id) {
+                        return Object.assign({}, item, {
+                            boardId: action.newBoardId
+                        })
+                    }
+                })
+            });
+
+        default:
+            return state;
+    }
+}
